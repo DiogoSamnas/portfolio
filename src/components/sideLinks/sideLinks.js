c05735567ae032cb0cd3a0662816064540413b32
DiogoSamnas <diogo.sam.nascimento@gmail.com>
2021-04-30 22:06:46 -0300

    Ajuste no redirecionamento dos links verticais

--- src/components/sideLinks/sideLinks.js
+++ src/components/sideLinks/sideLinks.js
@@ -5,12 +5,12 @@ function SideLinks() {
     return (
         <>
             <div id="profiles" className="d-none d-lg-flex">
-                <i><FiGithub/></i>
-                <i><FiLinkedin/></i>
-                <i><FiInstagram/></i>
+                <a href="https://github.com/DiogoSamnas" rel="noreferrer" target="_blank"><FiGithub/></a>
+                <a href="https://www.instagram.com/diogoodiego/" rel="noreferrer" target="_blank"><FiInstagram/></a>
+                <a href="https://www.linkedin.com/in/diogo-santos-b23ab31a0/" rel="noreferrer" target="_blank" ><FiLinkedin/></a>
             </div>
             <div id="email">
-                <p>diogo.sam.nascimento@gmail.com</p>
+                <a href="mailto:diogo.sam.nascimento@gmail.com"><p>diogo.sam.nascimento@gmail.com</p></a>
             </div>
         </>
     )
