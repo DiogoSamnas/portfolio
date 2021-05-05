4b175c90e71913800065c66edc534bbcb56a1545
DiogoSamnas <diogo.sam.nascimento@gmail.com>
2021-05-04 23:27:24 -0300

    Atualização dos links dos projetos

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -9,7 +9,7 @@ function Footer() {
             <p className="">Projetado e construído por Diogo Santos</p>
             <div className="social-links mt-2">
                 <a data-tip="Visitar respositório no Github" className="me-5" href="https://github.com/DiogoSamnas/portfolio" rel="noreferrer" target="_blank"><FiGithub /></a>
-                <a data-tip="Visitar os protótipos no Figma" href="https://www.figma.com/file/3MTbXIiWIWHOgKFH5vBKoB/Ui-Projects?node-id=33:0" rel="noreferrer" target="_blank"><FiFigma /></a>
+                <a data-tip="Visitar os protótipos no Figma" href="https://www.figma.com/file/EJqF1Gluh0aZ7P2u6xHO3R/Portfolio?node-id=1:3" rel="noreferrer" target="_blank"><FiFigma /></a>
             </div>
             <ReactTooltip />
         </div>
