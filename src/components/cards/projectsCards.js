647461352be3df295d91189a2f62ee87e03ec022
DiogoSamnas <diogo.sam.nascimento@gmail.com>
2021-06-02 18:36:58 -0300

    desabilitando botão visualizar mais projetos

--- src/components/cards/projectsCards.js
+++ src/components/cards/projectsCards.js
@@ -5,10 +5,9 @@ function ProjectsCards() {
     return (
         <>
             {data.map((data) => <Card key={data.id}  item={data}/>)}
-            <div class="my-card more-cards d-flex justify-content-center align-items-center" data-aos="fade-up" style={{backgroundColor:"#f7f5f5"}}>
-                <div className="nav position-relative">
-                    <button className="btn btn-underline">Visualizar mais</button>
-                </div>
+            <div class="my-card more-cards d-flex flex-column justify-content-center align-items-center" data-aos="fade-up" style={{backgroundColor:"#f7f5f5"}}>
+                    <h6 >Visualizar mais</h6>
+                    <span>Em breve!</span>
             </div>
         </>
     )
